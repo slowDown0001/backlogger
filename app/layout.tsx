57d4ca1ae3b195f06493f3bc9098f326023d55be
Sergei <ievlevsergei@gmail.com>
2025-05-24 15:24:13 +0800

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,17 +62,17 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center border-2 border-green-100 overflow-x-auto">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center border-2 border-green-100">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 mb-20 border-2 border-red-500">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm border-2 border-red-500">
-                  <div className="flex gap-5 items-center font-semibold border-2 border-red-500">
+          <main className="min-h-screen flex flex-col items-center overflow-x-auto">
+            <div className="flex-1 w-full flex flex-col gap-20 items-center border-2">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 mb-20">
+                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                  <div className="flex gap-5 items-center font-semibold border-red-500">
                     <Link href={"/"}>Backloger app for Ёж ЕГЭ</Link>
                   </div>
                   {isLoggedIn ? (
                     <div className="flex items-center gap-4">
                       <ProfileButton userId={user?.id} email={user?.email} />
-                      <WorkspaceDropdown currentWorkspace={currentWorkspace} workspaces={workspaces || []} />
+                      {/* <WorkspaceDropdown currentWorkspace={currentWorkspace} workspaces={workspaces || []} /> */}
                       <CreateButton />
                       <form action={signOutAction}>
                         <button type="submit" className="text-sm text-foreground underline">
@@ -82,20 +82,20 @@ export default async function RootLayout({
                       {!hasEnvVars ? <EnvVarWarning /> : null}
                     </div>
                   ) : (
-                    <div className="flex flex-col gap-20 max-w-5xl p-5 border-2 border-red-500">
-                      {children}
+                    <div className="flex items-center gap-4">
+                      {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                     </div>
                   )}
                 </div>
               </nav>
 
 
-              <div className="flex-1 w-full flex flex-col border-red-500">
+              <div className="flex-1 w-full flex flex-col items-center justify-center">
                 {children}
               </div>
 
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8 border-2 border-red-500">
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-8 border-2">
                 <p>Powered by Ёж</p>
                 <ThemeSwitcher />
               </footer>
